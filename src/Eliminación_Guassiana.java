88af494e14f779a389bce09a0bd5153ffe59bec4
Abraham Perez <abraham Perez Quiroz>
2023-11-12 15:22:46 -0600

    02 Correción de Matriz y multiplicacion de filas

--- src/Eliminación_Guassiana.java
+++ src/Eliminación_Guassiana.java
@@ -1,43 +1,37 @@
-import java.util.Scanner;
-
 public class Eliminación_Guassiana {
 
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        int filas=0;
-        int columnas=0;
+        int[][] matriz = {
+                {1, 4, 2, 11},
+                {3, 1, 2, 5},
+                {2, 3, 2, 12}
+        };
 
-        System.out.println("Eliminación gaussiana");
 
-        System.out.println("Creación de la matriz");
+        int filas = matriz.length;
+        int columnas = matriz[0].length-1;
 
-        System.out.println("Ingresa el número de filas");
-        filas=scanner.nextInt();
-        System.out.println("Ingresa el número de columnas   (Tiene que ser igual a las filas)");
-        columnas=scanner.nextInt();
 
-        if(filas==columnas){
-            int[][] matriz=new int[filas][columnas];
 
-            for(int i=0; i<matriz.length; i++){
-                for(int j=0; j<matriz[i].length; j++){
-                    System.out.println("Ingresa los valores de la matirz [ "+i+" ][ "+j+" ]");
-                    matriz[i][j]=scanner.nextInt();
-                }
+        for (int i = 0; i<filas; i++) {
+            for (int j=0; j<columnas+1; j++) {
+                System.out.print(matriz[i][j] + "|");
             }
-
-
-            for(int i=0; i< matriz.length; i++){
-                for(int j=0; j<matriz[i].length; j++){
-                    System.out.print(matriz[i][j]+"|");
-                }
-                System.out.println();
-            }
-
-
-        }else{
-            System.out.println("Las filas y columnas tienen que ser iguales");
+            System.out.println();
         }
+
+            for (int j=0; j<columnas+1; j++) {
+                matriz[0][j]*=-3+matriz[1][j];
+            }
+
+        System.out.println("-----------------");
+        for (int i = 0; i<filas; i++) {
+            for (int j=0; j<columnas+1; j++) {
+                System.out.print(matriz[i][j] + "|");
+            }
+            System.out.println();
+        }
+
     }
 
 }
