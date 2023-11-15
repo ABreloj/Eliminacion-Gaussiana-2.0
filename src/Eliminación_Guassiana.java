6c7ca3a593438eb8af2c3c1ee0f5b13e56f72ea9
Abraham Perez <abraham Perez Quiroz>
2023-11-15 06:54:36 -0600

    02 Corrección de errores

--- src/Eliminación_Guassiana.java
+++ src/Eliminación_Guassiana.java
@@ -1,67 +1,69 @@
 import java.util.Scanner;
 public class Eliminación_Guassiana {
 
-    public static void muestraMatriz(float matriz[][], int var) {
-        for (int i = 0; i < var; i++) {
-            for (int j = 0; j < (var + 1); j++) {
+    public static void muestraMatriz(int matriz[][], int numValores) {
+        for (int i = 0; i < numValores; i++) {
+            for (int j = 0; j < (numValores + 1); j++) {
                 System.out.print(" " + matriz[i][j] + " |");
             }
             System.out.println("");
         }
     }
 
-    public static void pivote(float matriz[][], int piv, int var) {
-        float temp = matriz[piv][piv];
-        for (int i = 0; i < (var + 1); i++) {
-            matriz[piv][i] = matriz[piv][i] / temp;
+    public static void pivote(int matriz[][], int piv, int numValores) {
+        int valorPiv = matriz[piv][piv];
+        for (int i = 0; i < (numValores + 1); i++) {
+            matriz[piv][i] = matriz[piv][i] / valorPiv;
         }
     }
 
-    static void hacerCeros(float matriz[][], int piv, int var) {
-        for (int i = 0; i < var; i++) {
+    static void hacerCeros(int matriz[][], int piv, int numValores) {
+        for (int i = 0; i < numValores; i++) {
             if (i != piv) {
-                float c = matriz[i][piv];
-                for (int j = 0; j < (var + 1); j++)
+                int c = matriz[i][piv];
+                for (int j = 0; j < (numValores + 1); j++) {
                     matriz[i][j] = ((-1 * c) * matriz[piv][j]) + matriz[i][j];
+                }
             }
         }
     }
 
     public static void main(String args[]) {
-        Scanner leer = new Scanner(System.in);
-        int var = 0, piv = 0;
-        float matriz[][];
+        try (Scanner leer = new Scanner(System.in)) {
+            int piv = 0, nColumnas, nFilas;
+            int matriz[][];
+            System.out.println("*ELIMINACIÓN GAUSSIANA*");
+            System.out.println("Escribe el total de filas: ");
+            nFilas = leer.nextInt();
+            System.out.println("Escribe el total de columnas: ");
+            nColumnas = leer.nextInt();
+            matriz = new int[nFilas][nColumnas + 1];
 
-        System.out.println("*ELIMINACIÓN GAUSSIANA*");
-        System.out.println("¿Cuántas variables tiene la matriz?");
-        var = leer.nextInt();
-        matriz = new float[var][var + 1];
+            for (int i = 0; i < nFilas; i++) {
+                for (int j = 0; j < (nColumnas + 1); j++) {
+                    System.out.printf("Ingresa el coeficiente de la posición: A[%d][%d]: ", (i + 1), (j + 1));
+                    matriz[i][j] = leer.nextInt();
+                }
+            }
 
-        for (int i = 0; i < var; i++) {
-            for (int j = 0; j < (var + 1); j++) {
-                System.out.println("Ingresa el coeficiente de la posición: A[" + (i + 1) + "][" + (j + 1) + "]");
-                matriz[i][j] = leer.nextFloat();
+            for (int i = 0; i < nFilas; i++) {
+                pivote(matriz, piv, nFilas);
+
+                System.out.println("Renglon " + (i + 1) + " dividido por el pivote");
+                muestraMatriz(matriz, nFilas);
+                System.out.println("");
+
+                System.out.println("********");
+                hacerCeros(matriz, piv, nFilas);
+                muestraMatriz(matriz, nFilas);
+                System.out.println("");
+                piv++;
+            }
+
+            System.out.println("Resultados Finales:");
+            for (int i = 0; i < nFilas; i++) {
+                System.out.println("El resultado de la matriz " + (i + 1) + " es: " + matriz[i][nFilas]);
             }
         }
-
-        for (int i = 0; i < var; i++) {
-            pivote(matriz, piv, var);
-
-            System.out.println("Renglon " + (i + 1) + " dividido por el pivote");
-            muestraMatriz(matriz, var);
-            System.out.println("");
-
-            System.out.println("********");
-            hacerCeros(matriz, piv, var);
-            muestraMatriz(matriz, var);
-            System.out.println("");
-            piv++;
-        }
-
-        for (int i = 0; i < var; i++) {
-            System.out.println("La variable " + (i + 1) + " es: " + matriz[i][var]);
-        }
-
-        leer.close();
     }
 }
