ef70e0abfd57c182bb0bafa66a618b7483352573
Abraham Perez <abraham Perez Quiroz>
2023-11-14 21:09:35 -0600

    06 Correción y cambio a codigo de Isa

--- src/Eliminación_Guassiana.java
+++ src/Eliminación_Guassiana.java
@@ -1,50 +1,67 @@
+import java.util.Scanner;
 public class Eliminación_Guassiana {
 
-    public static void main(String[] args) {
-        int[][] matriz = {
-                {1, -4, -2, -11},
-                {3, -1, 2, 5},
-                {2, 3, 2, 12}
-        };
-
-
-        int filas = matriz.length;
-        int columnas = matriz[0].length-1;
-
-
-
-        for (int i = 0; i<filas; i++) {
-            for (int j=0; j<columnas+1; j++) {
-                System.out.print(matriz[i][j] + "|");
+    public static void muestraMatriz(float matriz[][], int var) {
+        for (int i = 0; i < var; i++) {
+            for (int j = 0; j < (var + 1); j++) {
+                System.out.print(" " + matriz[i][j] + " |");
             }
-            System.out.println();
+            System.out.println("");
         }
-
-        for (int j=0; j<columnas+1; j++) {
-            matriz[1][j]=matriz[0][j]*-3+matriz[1][j];
-            matriz[2][j]=matriz[0][j]*-2+matriz[2][j];
-        }
-
-
-        System.out.println("--------------------------------------");
-        for (int i = 0; i<filas; i++) {
-            for (int j=0; j<columnas+1; j++) {
-                System.out.print(matriz[i][j] + "|");
-            }
-            System.out.println();
-        }
-
-        for(int j=0; j<columnas+1; j++){
-            matriz[2][j]=matriz[1][j]*-1+matriz[2][j];
-        }
-        System.out.println("--------------------------------------");
-        for (int i = 0; i<filas; i++) {
-            for (int j=0; j<columnas+1; j++) {
-                System.out.print(matriz[i][j] + "|");
-            }
-            System.out.println();
-        }
-
     }
 
+    public static void pivote(float matriz[][], int piv, int var) {
+        float temp = matriz[piv][piv];
+        for (int i = 0; i < (var + 1); i++) {
+            matriz[piv][i] = matriz[piv][i] / temp;
+        }
+    }
+
+    static void hacerCeros(float matriz[][], int piv, int var) {
+        for (int i = 0; i < var; i++) {
+            if (i != piv) {
+                float c = matriz[i][piv];
+                for (int j = 0; j < (var + 1); j++)
+                    matriz[i][j] = ((-1 * c) * matriz[piv][j]) + matriz[i][j];
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner leer = new Scanner(System.in);
+        int var = 0, piv = 0;
+        float matriz[][];
+
+        System.out.println("*ELIMINACIÓN GAUSSIANA*");
+        System.out.println("¿Cuántas variables tiene la matriz?");
+        var = leer.nextInt();
+        matriz = new float[var][var + 1];
+
+        for (int i = 0; i < var; i++) {
+            for (int j = 0; j < (var + 1); j++) {
+                System.out.println("Ingresa el coeficiente de la posición: A[" + (i + 1) + "][" + (j + 1) + "]");
+                matriz[i][j] = leer.nextFloat();
+            }
+        }
+
+        for (int i = 0; i < var; i++) {
+            pivote(matriz, piv, var);
+
+            System.out.println("Renglon " + (i + 1) + " dividido por el pivote");
+            muestraMatriz(matriz, var);
+            System.out.println("");
+
+            System.out.println("********");
+            hacerCeros(matriz, piv, var);
+            muestraMatriz(matriz, var);
+            System.out.println("");
+            piv++;
+        }
+
+        for (int i = 0; i < var; i++) {
+            System.out.println("La variable " + (i + 1) + " es: " + matriz[i][var]);
+        }
+
+        leer.close();
+    }
 }
